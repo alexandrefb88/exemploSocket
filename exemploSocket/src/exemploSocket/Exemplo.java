8c414d634dea7b594d149a130c1f341440fec461
Alexandre <Usuario@10.21.82.51>
2017-03-21 12:16:01 -0300

    Final

--- exemploSocket/src/exemploSocket/Exemplo.java
+++ exemploSocket/src/exemploSocket/Exemplo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exemploSocket;
 
 import java.io.IOException;
@@ -12,17 +7,10 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Francisco
- */
 public class Exemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(9000);){
+        try (ServerSocket server = new ServerSocket(8090);){
             
             Socket socket = server.accept();
             Scanner scan = new Scanner(socket.getInputStream());
